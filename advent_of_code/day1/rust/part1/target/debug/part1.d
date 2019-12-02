03fa197b153f760ef1be78199d78400113db2de0
Bilal Yavuz <byavuz81@gmail.com>
2019-12-03 01:11:58 +0300

    Removing target

--- advent_of_code/day1/rust/part1/target/debug/part1.d
+++
@@ -1,1 +0,0 @@
-/home/nbyavuz/CompSolutions/advent_of_code/day1/rust/part1/target/debug/part1: /home/nbyavuz/CompSolutions/advent_of_code/day1/rust/part1/src/main.rs
--- advent_of_code/day1/rust/part2/target/debug/part2.d
+++
@@ -1,1 +0,0 @@
-/home/nbyavuz/CompSolutions/advent_of_code/day1/rust/part2/target/debug/part2: /home/nbyavuz/CompSolutions/advent_of_code/day1/rust/part2/src/main.rs
